3144c8a8b9dc00e33334b85ac1ef27b0b91aa48a
Carlos Santos <carlossantosdm@gmail.com>
2022-03-19 02:17:46 -0300

    add api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { categoriesRoutes } from './routes/categories.router';
 import { specificationsRoutes } from './routes/specifications.router';
+
+import swaggerFile from './swagger.json';
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/categories", categoriesRoutes);
 app.use("/specifications", specificationsRoutes);
