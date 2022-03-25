3b063a552e7c1912d05520bad4ab300f7ec1b4bc
Carlos Santos <carlossantosdm@gmail.com>
2022-03-24 22:37:38 -0300

    add middlewares

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from "express";
+
+
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    const token = request.headers;
+}
